d47dd5835fd135bbbe62b4daac74bbbe0eecdae5
Timon Engelke <timon.engelke@dlr.de>
2024-04-15 11:32:23 +0200

    Remove C++ Code

--- src/benchmark_logger.cpp
+++
@@ -1,2 +0,0 @@
-#include "benchmark_logger.hpp"
-
--- src/plugins.cpp
+++
@@ -1,5 +0,0 @@
-#include "reachability_evaluator.hpp"
-#include "benchmark_moveit_ik_solver.hpp"
-#include <reach/plugin_utils.h>
-EXPORT_IK_SOLVER_PLUGIN(benchmark::MoveItIKSolverFactory, MoveItIKSolver2)
-EXPORT_EVALUATOR_PLUGIN(benchmark::ReachabilityEvaluatorFactory, ReachabilityEvaluator)
