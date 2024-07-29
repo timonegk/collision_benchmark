ef4d89cfe1d46aac4113e3176aa1112fc8843bf5
Timon Engelke <timon.engelke@dlr.de>
2024-07-29 10:13:21 +0200

    update benchmark script

--- scripts/run_benchmark.py
+++ scripts/run_benchmark.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python
 from ebike.benchmark import Benchmark
-from ebike.ik import KDL, TracIK, PickIK, BioIK, RelaxedIK
-from ebike.scenario import Table, SmallTable, Kallax, Barrel
-from collision_benchmark.scenario import Wing, TestEntry
+from ebike.ik import KDL, TracIK, TracIKDistance, PickIK, BioIK, RelaxedIK, BioIK1, BioIK2MemeticL, BioIK2MemeticLBFGS, BioIK2, \
+    BioIKGDR, BioIKGDR8, BioIKJac, BioIKJac8, BioIKOptlibBFGS, BioIKOptlibBFGS4, BioIKRCM, BioIKDepth, BioIKCollisionDistance, RelaxedIKRCM, RelaxedIKDepth, RelaxedIKCollisionDistance
+from ebike.scenario import Table, SmallTable, Kallax, Barrel, Random
+from collision_benchmark.scenario import Wing, TestEntrySmall, TestEntry
 from collision_benchmark.robot import Elise
 
 if __name__ == '__main__':
-    benchmark = Benchmark()
+    benchmark = Benchmark(interactive=False)
     #benchmark.add_ik(KDL())
     #benchmark.add_ik(TracIK())
     #benchmark.add_ik(PickIK())
     #benchmark.add_ik(BioIK())
-    benchmark.add_ik(RelaxedIK())
-    benchmark.add_scenario(TestEntry())
+    #benchmark.add_ik(BioIKRCM())
+    #benchmark.add_ik(BioIKDepth())
+    benchmark.add_ik(BioIKCollisionDistance())
+    #benchmark.add_ik(RelaxedIK())
+    #benchmark.add_ik(RelaxedIKRCM())
+    #benchmark.add_ik(RelaxedIKDepth())
+    benchmark.add_ik(RelaxedIKCollisionDistance())
+    #benchmark.add_scenario(Random())
+    benchmark.add_scenario(TestEntrySmall())
     benchmark.add_robot(Elise())
     benchmark.run()
     benchmark.print()
-    benchmark.plot()
