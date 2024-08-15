35a4e6723d49b60a3198a5a3ac7d32ca6cf5d49a
Timon Engelke <timon.engelke@dlr.de>
2024-08-15 11:14:03 +0200

    Update hydrogen tank seed

--- collision_benchmark/scenario.py
+++ collision_benchmark/scenario.py
@@ -14,12 +14,12 @@ class HydrogenTankSeed(HydrogenTank):
     def get_config(self, *args):
         config = super().get_config(*args)
         config["optimization"]["seed_state"] = [
-                {"name": "shoulder_pan_joint", "position": 5.03},
+                {"name": "shoulder_pan_joint", "position": -1.25},
                 {"name": "shoulder_lift_joint", "position": -1.69},
                 {"name": "elbow_joint", "position": -2.22},
-                {"name": "wrist_1_joint", "position": -5.54},
-                {"name": "wrist_2_joint", "position": -5.0},
-                {"name": "wrist_3_joint", "position": -4.05},
+                {"name": "wrist_1_joint", "position": 0.74},
+                {"name": "wrist_2_joint", "position": 1.28},
+                {"name": "wrist_3_joint", "position": 0.0},
                 {"name": "endo_first_joint", "position": 0.0},
                 {"name": "endo_second_joint_first_dof", "position": 0.0},
                 {"name": "endo_second_joint_second_dof", "position": 0.0},
