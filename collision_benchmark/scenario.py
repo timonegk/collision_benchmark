0a91aecc4992b45d3dea4c6480ed4a25d83b5fdf
Timon Engelke <timon.engelke@dlr.de>
2024-09-01 12:41:00 +0200

    Update seeds for HydrogenTank

--- collision_benchmark/scenario.py
+++ collision_benchmark/scenario.py
@@ -14,15 +14,33 @@ class HydrogenTankSeed(HydrogenTank):
     def get_config(self, *args):
         config = super().get_config(*args)
         config["optimization"]["seed_state"] = [
-                {"name": "shoulder_pan_joint", "position": -1.25},
-                {"name": "shoulder_lift_joint", "position": -1.69},
-                {"name": "elbow_joint", "position": -2.22},
-                {"name": "wrist_1_joint", "position": 0.74},
-                {"name": "wrist_2_joint", "position": 1.28},
+                {"name": "shoulder_pan_joint", "position": 1.134},
+                {"name": "shoulder_lift_joint", "position": -1.478},
+                {"name": "elbow_joint", "position": 2.438},
+                {"name": "wrist_1_joint", "position": 2.070},
+                {"name": "wrist_2_joint", "position": -1.138},
                 {"name": "wrist_3_joint", "position": 0.0},
                 {"name": "endo_first_joint", "position": 0.0},
                 {"name": "endo_second_joint_first_dof", "position": 0.0},
                 {"name": "endo_second_joint_second_dof", "position": 0.0},
+        ]
+        return config
+
+
+class HydrogenTankSeed2(HydrogenTank):
+    name = 'hydrogen_tank_seed2'
+    def get_config(self, *args):
+        config = super().get_config(*args)
+        config["optimization"]["seed_state"] = [
+                {"name": "shoulder_pan_joint", "position": 1.134},
+                {"name": "shoulder_lift_joint", "position": -1.478},
+                {"name": "elbow_joint", "position": 2.438},
+                {"name": "wrist_1_joint", "position": 2.070},
+                {"name": "wrist_2_joint", "position": -1.138},
+                {"name": "wrist_3_joint", "position": 3.143},
+                {"name": "endo_first_joint", "position": -0.288},
+                {"name": "endo_second_joint_first_dof", "position": 0.125},
+                {"name": "endo_second_joint_second_dof", "position": -1.386},
         ]
         return config
 
